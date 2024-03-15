de3d59dfa7c7ca993f36420b4d57b63cdcec16f9
takiido <157808651+takiido@users.noreply.github.com>
2024-03-14 23:11:36 -0500

    try to read user dir

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+void ReadConfig();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ReadConfig();
     return 0;
+}
+
+void ReadConfig() {
+    std::string path = "~/";
+    for (const auto & entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
 }
