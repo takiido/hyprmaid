928e1b3af30eeb5def8878f6e42e30041555e9fa
takiido <157808651+takiido@users.noreply.github.com>
2024-03-14 23:35:40 -0500

    try to create config file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <filesystem>
-namespace fs = std::filesystem;
+#include <fstream>
+//namespace fs = std::filesystem;
 
 void ReadConfig();
 
@@ -10,7 +11,13 @@ int main() {
 }
 
 void ReadConfig() {
-    std::string home_dir = getenv("HOME");
-    home_dir += "/";
-    std::cout << home_dir << std::endl;
+    std::string filename = getenv("HOME");
+    filename += "/.config/hyprmaid/hyprmaid.conf";
+
+    std::fstream fs;
+    fs.open(filename, std::ios::out | std::ios::app);
+    fs.close();
+    fs.open(filename, std::ios::in | std::ios::out | std::ios::app);
+
+    std::cout << filename << std::endl;
 }
