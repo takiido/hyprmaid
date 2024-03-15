441814764e4056a6c06dc748e816fc52b3fddac1
takiido <157808651+takiido@users.noreply.github.com>
2024-03-15 01:00:43 -0500

    initial commit

---
+++ img_proc/img_proc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by takiido on 3/15/2024.
+//
+
+#include "img_proc.h"
---
+++ img_proc/img_proc.h
@@ -0,0 +1,14 @@
+//
+// Created by takiido on 3/15/2024.
+//
+
+#ifndef HYPRMAID_IMG_PROC_H
+#define HYPRMAID_IMG_PROC_H
+
+
+class img_proc {
+
+};
+
+
+#endif //HYPRMAID_IMG_PROC_H
